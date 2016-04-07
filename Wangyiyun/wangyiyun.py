9ce4436431a8abe4fab5ac1015a04b3cc887987c
wanggaobin <313085945@qq.com>
2016-04-07 17:16:30 +0800

    crawler

--- Wangyiyun/wangyiyun.py
+++ Wangyiyun/wangyiyun.py
@@ -12,9 +12,10 @@ log_file_name="Log.txt"
 
 #url="http://lishi.tianqi.com/hangzhou/201101.html"
 sound_ids=[36025962,34057518,32924132]
-n1=1
+n1=50000000
 n2=1000000000
 sound_ids=range(n1,n2)
+#sound_ids=[n1+1]
 for sound_id  in sound_ids:
    try:
       url="http://music.163.com/song?id="+str(sound_id)
@@ -22,11 +23,13 @@ for sound_id  in sound_ids:
       html=html.decode("utf-8")
       soup=BeautifulSoup(html)
       d=soup.find("em",{"class":"f-ff2"})
-      d=str(d)
+      d=str(type(d))
       if  d=="<class 'NoneType'>":
-            continue
+         continue
       sound_name=soup.find("em",{"class":"f-ff2"}).text
+      print(sound_name)
       temp1=soup.findAll("p",{"class":"des s-fc4"})[0]
+      print(temp1)
       singer_url="http://music.163.com/#"+str(temp1.find("a",{"class":"s-fc7"})["href"])
       singer=temp1.find("a",{"class":"s-fc7"}).text
       temp2=soup.findAll("p",{"class":"des s-fc4"})[1]
@@ -39,8 +42,7 @@ for sound_id  in sound_ids:
       f=open(log_file_name,"a")
       datetime1=time.strftime('%Y-%m-%d',time.localtime(time.time()))
       datetime2=time.strftime('%H:%M:%S',time.localtime(time.time()))
-      f.write("###"+"error sound_id:"+str(sound_id)+"###"+"error_time"+datetime1+" "+datetime2+'\n')
-      #.write(error_list)
+      f.write("###"+"error sound_id:"+str(sound_id)+"###"+"error_time"+datetime1+" "+datetime2)
       traceback.print_exc(file=f)
       f.flush()
       f.close()
