bf720274993d4a2d1164d213aafa0aa5d7f6e795
wanggaobin <313085945@qq.com>
2016-04-07 15:47:19 +0800

    crawler

--- Wangyiyun/wangyiyun.py
+++ Wangyiyun/wangyiyun.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+import trace
 import urllib
 import time
 from urllib import request
 from bs4 import BeautifulSoup 
 import re
+import datetime
+
+log_file_name="Log.txt"
 
 #url="http://lishi.tianqi.com/hangzhou/201101.html"
 sound_ids=[36025962,34057518,32924132]
@@ -30,3 +34,14 @@ for sound_id  in sound_ids:
       channel=temp2.find("a",{"class":"s-fc7"}).text
       comments=soup.find("span",{"id":"cnt_comment_count"}).text
       print(sound_id,",",sound_name,",",singer,",",singer_url,",",channel,",",channel_url,",",comments)
+   except:
+      #eror_list.append(sound_id)
+      f=open(log_file_name,"a")
+      datetime1=time.strftime('%Y-%m-%d',time.localtime(time.time()))
+      datetime2=time.strftime('%H:%M:%S',time.localtime(time.time()))
+      f.write("###"+"error sound_id:"+str(sound_id)+"###"+"error_time"+datetime1+" "+datetime2+'\n')
+      #.write(error_list)
+      traceback.print_exc(file=f)
+      f.flush()
+      f.close()
+
